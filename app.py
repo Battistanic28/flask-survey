87bd394987bec21b16125bf0ded02d4d46e400d6
Nick Battista <nickbattista@Nick.Battista>
2020-12-30 11:18:11 -0500

    added response handling

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'secret'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 responses = []
@@ -19,4 +20,19 @@ def home_page():
 @app.route('/questions/<int:id>')
 def render_question(id):
     questions = satisfaction_survey.questions
+    if (len(responses) != id):
+        flash (f"Invalid question ID:{id}")
+        return redirect(f"/questions/{len(responses)}")
     return render_template('questions.html', questions=questions, id=id)
+
+@app.route('/answer', methods=["POST"])
+def get_response():
+    choice = request.form['answer']
+    responses.append(choice)
+
+    if (len(responses) == len(satisfaction_survey.questions)):
+        return render_template('complete.html', responses=responses)
+
+    else:
+        return redirect(f'/questions/{len(responses)}')
+    
